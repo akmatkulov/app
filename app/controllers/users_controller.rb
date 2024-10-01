118339b38d8d28ae450d38638da4f5df64c4001a
sovetbek <pnz19620@gmail.com>
2024-10-01 16:42:55 +0600

    Make a basic signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,24 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[ show ]
   def new
+    @user = User.new
   end
+  def show; end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Instagram!"
+      redirect_to @user
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:user_name, :name, :email, :password, :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root "pages#home"
-  get "/users/new"
+  get "signup", to: "users#new"
+  resources :users
 end
