34eb67f4657217006e39d133d53f4f8b8b827fda
sovetbek <pnz19620@gmail.com>
2024-10-05 18:32:53 +0600

    Add remember

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,18 +1,45 @@
 module SessionsHelper
+  # Save user id in session
   def log_in(user)
     session[:user_id] = user.id
   end
 
+  # Return current user of session or cookies
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+        @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
+  # Return true or false if present
   def logged_in?
     !!current_user
   end
 
+  # Remember user and token in cookies and database
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # Delete user in session
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  # Delete cookies
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
   end
 end
