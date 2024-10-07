a0f8535783d3055c2c7520c90fe0efb30653b35b
sovetbek <pnz19620@gmail.com>
2024-10-07 16:05:29 +0600

    Add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_create :create_activation_digest
 
   normalizes :email, with: ->(email) { email.downcase }
@@ -29,6 +29,17 @@ class User < ApplicationRecord
     SecureRandom.urlsafe_base64
   end
 
+  # Set token for attribute and digest for db
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
   # Remember user in database
   def remember
     self.remember_token = User.new_token
@@ -56,6 +67,10 @@ class User < ApplicationRecord
   # Sends activation email.
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,11 @@ Rails.application.routes.draw do
   get "signup", to: "users#new"
   resources :users
 
-  # account
+  # account activations
   resources :account_activations, only: %i[ edit ]
+
+  # password resets
+  resources :password_resets, only: %i[ new create edit update]
 
   # sessions
   get "login", to: "sessions#new"
